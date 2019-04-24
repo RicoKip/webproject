6473d0392acca3bd1e68fb2303115b3c98f07711
Raptor <erickipkemoi17@gmail.com>
2019-04-24 05:44:13 +0100

    HTML, CSS, PHP implementation

--- includes/registerForm.php
+++ includes/registerForm.php
@@ -1,4 +1,17 @@
 <!DOCTYPE html>
+<!--
+Created by: Eric Kipkemoi
+Edited and referenced other code implementation
+Outsourced images used in the project
+References: 
+W3S - Login and Registration forms and processes, Responsive web deisigns image file upload, grid view and
+box sizing properties. Web styling.
+StackOverflow - Google maps API implementation, Login and registration implementation, Apache server 
+web programming implementation.
+YouTube tutorials - server user login and registration with sessions, image database uploads, Google Maps
+geocoding and implementation, database storage implementation using php and mysql, embedded web javascript
+Links to the reference criteria available in the project report
+-->
 <!--action: process to server tor register user-->
 <form action="register.php" method="post" enctype="multipart/form-data">
     <div class="containerLgn">
--- processes/geocode.php
+++ processes/geocode.php
@@ -1,3 +1,16 @@
+<!--
+Created by: Eric Kipkemoi
+Edited and referenced other code implementation
+Outsourced images used in the project
+References: 
+W3S - Login and Registration forms and processes, Responsive web deisigns image file upload, grid view and
+box sizing properties. Web styling.
+StackOverflow - Google maps API implementation, Login and registration implementation, Apache server 
+web programming implementation.
+YouTube tutorials - server user login and registration with sessions, image database uploads, Google Maps
+geocoding and implementation, database storage implementation using php and mysql, embedded web javascript
+Links to the reference criteria available in the project report
+-->
 <?php
 //establich database connection
 include ('connection.php');
